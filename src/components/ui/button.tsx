fe81238c16614f8b18fe0760e3cf8bdd6cd73542
Bruno Fabre <bruno.hfabre@gmail.com>
2024-09-14 07:19:17 -0300

    fix

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
-import { Slot } from '@radix-ui/react-slot'
+
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
+import { Slot } from '@radix-ui/react-slot'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -24,14 +25,14 @@ const buttonVariants = cva(
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
-        icon: 'h-9 w-9',
+        icon: 'size-9',
       },
     },
     defaultVariants: {
       variant: 'default',
       size: 'default',
     },
-  }
+  },
 )
 
 export interface ButtonProps
@@ -50,7 +51,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     )
-  }
+  },
 )
 Button.displayName = 'Button'
 
--- src/pages/collections/collection/request.tsx
+++ src/pages/collections/collection/request.tsx
@@ -1,3 +1,8 @@
+import { Controller, useFieldArray, useForm, useWatch } from 'react-hook-form'
+
+import { X } from 'lucide-react'
+import { z } from 'zod'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import {
@@ -9,33 +14,85 @@ import {
 } from '@/components/ui/select'
 import { Separator } from '@/components/ui/separator'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { z } from 'zod'
-import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import Editor from '@monaco-editor/react'
 
 const formSchema = z.object({
   method: z.enum(['get', 'post', 'put', 'patch', 'delete']),
   url: z.string(),
+
+  bodyType: z.enum(['none', 'json']),
+  body: z.string().optional(),
+
+  authType: z.enum(['none', 'bearer']),
+  auth: z.record(z.string(), z.string()).optional(),
+
+  headers: z.array(
+    z.object({
+      name: z.string(),
+      value: z.string(),
+    }),
+  ),
+
+  params: z.array(
+    z.object({
+      name: z.string(),
+      value: z.string(),
+    }),
+  ),
 })
 
 type FormData = z.infer<typeof formSchema>
+
+// const controller = new AbortController()
 
 export function Request() {
   const form = useForm<FormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       method: 'get',
+
+      bodyType: 'none',
+
+      authType: 'none',
     },
   })
+  const headersField = useFieldArray({
+    control: form.control,
+    name: 'headers',
+  })
+  const paramsField = useFieldArray({
+    control: form.control,
+    name: 'params',
+  })
+
+  const bodyType = useWatch({ control: form.control, name: 'bodyType' })
+  const authType = useWatch({ control: form.control, name: 'authType' })
 
   function sendRequest(data: FormData) {
     console.log(data)
+    // axios({
+    //   method,
+    //   url: routeInput,
+    //   signal: controller.signal,
+    //   headers: finalHeaders,
+    //   data: body ? JSON.parse(body) : undefined,
+    // })
+    //   .then((data) => {
+    //     setResponse(data)
+    //   })
+    //   .catch((error) => {
+    //     setResponse(error.response)
+    //   })
+    //   .finally(() => {
+    //     setLoading(false)
+    //   })
   }
 
   return (
-    <div className="flex-1 flex flex-col">
+    <div className="flex flex-1 flex-col">
       <form
-        className="flex p-2 gap-2"
+        className="flex gap-2 p-2"
         onSubmit={form.handleSubmit(sendRequest)}
       >
         <Controller
@@ -43,7 +100,7 @@ export function Request() {
           name="method"
           render={({ field }) => (
             <Select value={field.value} onValueChange={field.onChange}>
-              <SelectTrigger className="min-w-24 w-min">
+              <SelectTrigger className="w-min min-w-24">
                 <SelectValue placeholder="Method" />
               </SelectTrigger>
 
@@ -65,30 +122,174 @@ export function Request() {
 
       <Separator orientation="horizontal" />
 
-      <Tabs defaultValue="body" className="flex-1 flex flex-col">
+      <Tabs defaultValue="body" className="flex flex-1 flex-col">
         <div className="p-2">
           <TabsList>
             <TabsTrigger value="body">Body</TabsTrigger>
             <TabsTrigger value="auth">Auth</TabsTrigger>
-            <TabsTrigger value="params">Params</TabsTrigger>
             <TabsTrigger value="headers">Headers</TabsTrigger>
+            <TabsTrigger value="params">Params</TabsTrigger>
           </TabsList>
         </div>
 
-        <TabsContent value="body" className="m-0 flex-1 bg-red-200">
-          <span>body</span>
+        <TabsContent value="body" className="m-0" asChild>
+          <div className="flex flex-1 flex-col">
+            <div className="flex-1">
+              {bodyType === 'json' && (
+                <Controller
+                  control={form.control}
+                  name="body"
+                  render={({ field }) => (
+                    <Editor
+                      defaultLanguage="json"
+                      value={field.value}
+                      onChange={(value) => field.onChange(value)}
+                    />
+                  )}
+                />
+              )}
+            </div>
+
+            <div className="p-2">
+              <Controller
+                control={form.control}
+                name="bodyType"
+                render={({ field }) => (
+                  <Select
+                    value={field.value}
+                    onValueChange={(value) => {
+                      field.onChange(value)
+
+                      if (value === 'none') {
+                        form.setValue('body', '')
+                      }
+                    }}
+                  >
+                    <SelectTrigger className="w-min min-w-20">
+                      <SelectValue placeholder="Body type" />
+                    </SelectTrigger>
+
+                    <SelectContent>
+                      <SelectItem value="none">None</SelectItem>
+                      <SelectItem value="json">Json</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
+          </div>
         </TabsContent>
 
-        <TabsContent value="auth" className="m-0 flex-1 bg-violet-200">
-          <span>auth</span>
+        <TabsContent value="auth" className="m-0" asChild>
+          <div className="flex flex-1 flex-col">
+            <div className="flex-1 px-2 py-6">
+              {authType === 'bearer' && <Input placeholder="Token" />}
+            </div>
+
+            <div className="p-2">
+              <Controller
+                control={form.control}
+                name="authType"
+                render={({ field }) => (
+                  <Select
+                    value={field.value}
+                    onValueChange={(value) => {
+                      field.onChange(value)
+
+                      if (value === 'none') {
+                        form.setValue('auth', {})
+                      }
+                    }}
+                  >
+                    <SelectTrigger className="w-min min-w-24">
+                      <SelectValue placeholder="Auth type" />
+                    </SelectTrigger>
+
+                    <SelectContent>
+                      <SelectItem value="none">None</SelectItem>
+                      <SelectItem value="bearer">Bearer</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
+          </div>
         </TabsContent>
 
-        <TabsContent value="params" className="m-0 flex-1 bg-green-200">
-          <span>params</span>
+        <TabsContent value="headers" className="m-0" asChild>
+          <div className="flex flex-1 flex-col gap-4 p-2">
+            <div className="flex justify-end">
+              <Button
+                type="button"
+                onClick={() => headersField.append({ name: '', value: '' })}
+              >
+                + Header
+              </Button>
+            </div>
+
+            <div className="flex flex-col gap-2 overflow-auto">
+              {headersField.fields.map((field, index) => (
+                <div key={field.id} className="flex gap-2">
+                  <Input
+                    className="flex-1"
+                    placeholder="Name"
+                    {...form.register(`headers.${index}.name`)}
+                  />
+                  <Input
+                    className="flex-1"
+                    placeholder="Value"
+                    {...form.register(`headers.${index}.value`)}
+                  />
+                  <Button
+                    type="button"
+                    variant="outline"
+                    size="icon"
+                    onClick={() => headersField.remove(index)}
+                  >
+                    <X className="size-4" />
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </div>
         </TabsContent>
 
-        <TabsContent value="headers" className="m-0 flex-1 bg-blue-200">
-          <span>headers</span>
+        <TabsContent value="params" className="m-0" asChild>
+          <div className="flex flex-1 flex-col gap-4 p-2">
+            <div className="flex justify-end">
+              <Button
+                type="button"
+                onClick={() => paramsField.append({ name: '', value: '' })}
+              >
+                + Param
+              </Button>
+            </div>
+
+            <div className="flex flex-col gap-2 overflow-auto">
+              {paramsField.fields.map((field, index) => (
+                <div key={field.id} className="flex gap-2">
+                  <Input
+                    className="flex-1"
+                    placeholder="Name"
+                    {...form.register(`params.${index}.name`)}
+                  />
+                  <Input
+                    className="flex-1"
+                    placeholder="Value"
+                    {...form.register(`params.${index}.value`)}
+                  />
+                  <Button
+                    type="button"
+                    variant="outline"
+                    size="icon"
+                    onClick={() => paramsField.remove(index)}
+                  >
+                    <X className="size-4" />
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </div>
         </TabsContent>
       </Tabs>
     </div>
