63f95fcb44cc20cbd9f174eb891a5ba164b161cb
Bruno Fabre <bruno.hfabre@gmail.com>
2024-09-17 11:29:58 -0300

    add logo to sign in

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import { LoaderCircle } from 'lucide-react'
 import { z } from 'zod'
 
+import LogoImage from '@/assets/logo.png'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -44,7 +45,9 @@ export function SignIn() {
 
   return (
     <div className="mx-auto flex max-w-96 flex-1 flex-col justify-center gap-8">
-      <h1 className="text-2xl font-semibold">Sign in</h1>
+      <img src={LogoImage} alt="Mocha" className="w-10" />
+
+      <h1 className="text-xl font-semibold">Sign in</h1>
 
       <form
         onSubmit={form.handleSubmit(signIn)}
