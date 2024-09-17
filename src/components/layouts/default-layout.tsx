3c8c09fed10fe5307722b5e12460766b350f64aa
Bruno Fabre <bruno.hfabre@gmail.com>
2024-09-17 11:26:19 -0300

    add logo

--- src/components/layouts/default-layout.tsx
+++ src/components/layouts/default-layout.tsx
@@ -3,6 +3,7 @@ import { Navigate, Outlet, useNavigate } from 'react-router-dom'
 
 import { LogOut } from 'lucide-react'
 
+import LogoImage from '@/assets/logo.png'
 import { api } from '@/lib/api'
 import { CreateName } from '@/pages/create-name'
 import { useAuthStore } from '@/stores/auth-store'
@@ -48,10 +49,16 @@ export function DefaultLayout() {
 
   return (
     <div className="flex h-screen w-full antialiased">
-      <div className="flex items-end p-2">
-        <Button variant="outline" size="icon" onClick={handleSignOut}>
-          <LogOut className="size-4" />
-        </Button>
+      <div className="flex flex-col justify-between">
+        <div className="flex size-[52px] items-center justify-center">
+          <img src={LogoImage} alt="Mocha" className="w-9" />
+        </div>
+
+        <div className="flex size-[52px] items-center justify-center">
+          <Button variant="outline" size="icon" onClick={handleSignOut}>
+            <LogOut className="size-4" />
+          </Button>
+        </div>
       </div>
 
       <Separator orientation="vertical" />
