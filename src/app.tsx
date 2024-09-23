839b6e4136b5d920926a38054854017c334b63af
Bruno Fabre <bruno.hfabre@gmail.com>
2024-09-23 08:02:51 -0300

    add collections table

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,17 @@
 import { RouterProvider } from 'react-router-dom'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+
 import { ThemeProvider } from './components/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="mocha.ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="mocha.ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,14 +1,78 @@
+import { Empty } from '@/components/empty'
 import { Button } from '@/components/ui/button'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { api } from '@/lib/api'
+import { useOrganizationStore } from '@/stores/organization-store'
+import { useQuery } from '@tanstack/react-query'
+
+type Collection = {
+  id: string
+  name: string
+}
 
 export function Collections() {
+  const organization = useOrganizationStore((state) => state.organization)
+
+  const { data, isPending } = useQuery({
+    queryKey: ['collections'],
+    queryFn: async () => {
+      const response = await api.get<{ collections: Collection[] }>(
+        `/organizations/${organization?.id}/collections`,
+      )
+
+      return response.data.collections
+    },
+  })
+
+  if (isPending) {
+    return (
+      <div className="flex flex-1 items-center justify-center">
+        <span>is loading</span>
+      </div>
+    )
+  }
+
   return (
-    <div className="flex flex-1 flex-col gap-6 p-4">
+    <div className="flex flex-1 flex-col gap-4 p-4">
       <header className="flex justify-between">
-        <p className="text-lg font-medium">Collections</p>
+        <p className="text-lg font-semibold">Collections</p>
         <Button type="button">+ Collection</Button>
       </header>
 
-      <div className="flex-1 bg-red-200" />
+      {data?.length === 0 ? (
+        <Empty />
+      ) : (
+        <div className="flex flex-1 rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead>Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {data?.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>INV001</TableCell>
+                  <TableCell>Paid</TableCell>
+                  <TableCell>Credit Card</TableCell>
+                  <TableCell>$250.00</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      )}
     </div>
   )
 }
