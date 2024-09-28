136d88c93e7557f45739c8935333320adfc387e7
Bruno Fabre <bruno.hfabre@gmail.com>
2024-09-28 10:43:26 -0300

    fix

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,9 +1,9 @@
-export const debounce = (mainFunction, delay) => {
+export const debounce = (mainFunction: Function, delay: number) => {
   // Declare a variable called 'timer' to store the timer ID
-  let timer
+  let timer: any
 
   // Return an anonymous function that takes in any number of arguments
-  return function (...args) {
+  return function (...args: any) {
     // Clear the previous timer to prevent the execution of 'mainFunction'
     clearTimeout(timer)
 
